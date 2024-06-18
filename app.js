59732e2b618daab1026156f4b893d2b063a12da0
noviceGuru <amin.arabi@gmail.com>
2024-06-17 21:32:34 -0400

    problem solved

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('logged')
+const plusButton = document.getElementById('plusButton')
+const minusButton = document.getElementById('minusButton')
+const counterNumber = document.getElementById('counterNumber')
+
+let number = 0
+
+plusButton.onclick = () => {
+    number++
+    counterNumber.innerText= number
+}
+
+minusButton.onclick = () => {
+    number--
+    counterNumber.innerText= number
+}
